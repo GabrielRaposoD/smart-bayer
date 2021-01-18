f09c1367afdeddd8d413ffaae2aac21f53fb7356
kayle <kaylebarrt@users.noreply.github.com>
2021-01-18 18:55:31 -0300

    feat(texts): inserting correct texts

--- src/components/Steps/VideoType/index.tsx
+++ src/components/Steps/VideoType/index.tsx
@@ -1,8 +1,8 @@
 // Module Imports
-import React from 'react'
+import React from "react";
 
 // Components Import
-import { Logo, Stepper } from '@components/index'
+import { Logo, Stepper } from "@components/index";
 
 const VideoType: React.FC = () => {
   return (
@@ -10,22 +10,20 @@ const VideoType: React.FC = () => {
       <div className="flex flex-col">
         <Logo />
         <div className="mt-10">
-          <h1 className="text-4xl text-gray-800 leading-snug font-bold">Que tipo de vídeo você gostaria de criar?</h1>
-          <h3 className="mt-6 text-2xl text-gray-600 font-medium">Crie seu próprio vídeo de acompanhamento</h3>
-          <h3 className="text-2xl text-gray-600 font-medium">para compartilhar com seus clientes.</h3>
+          <h1 className="text-4xl font-bold leading-snug text-gray-800">
+            Que tipo de vídeo você gostaria de criar?
+          </h1>
+          <h3 className="mt-6 text-xl font-medium text-gray-600">
+            Escolha nas opções abaixo o que melhor te atende
+          </h3>
+          <h3 className="text-2xl font-medium text-gray-600">--input--</h3>
         </div>
       </div>
-      <div className="w-full">
-        <h6 className="tracking-wide text-base text-gray-500">
-          Ao clicar em Criar vídeo, toda a informação inserida e criação dos materiais é de total responsabilidade do
-          criador.
-        </h6>
-        <div className="mt-6">
-          <Stepper step={1} />
-        </div>
+      <div className="w-full mt-6">
+        <Stepper step={1} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export { VideoType }
+export { VideoType };
